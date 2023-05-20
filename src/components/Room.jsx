4ddbb2e6be4c6c5b2af459bd0d307189ed1b1189
Aurora <aurora.grippaudo@dotenv.it>
2023-05-20 03:44:01 +0200

    added features

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -5,16 +5,20 @@ import { useRouter } from "next/navigation";
 export default function Room({user}) {
   
 //Change rooms
-  function handleClick() {
-    router.push(`/${user.data.idroom}`);
-
+  function navigateRoom() {
+    if(user.data.status){
+      router.push(`/${user.data.idroom}`);
+    } else {
+      alert(`${user.data.name} is busy now, they may be free later.`)
+    }
   }
 
   const router = useRouter();
 
   return (
-        <div className="room rounded-lg mb-2" onClick={handleClick}>
-          <h2>{user.data.name}</h2>
+        <div className="room rounded-lg mb-2" onClick={navigateRoom}>
+          <h2 className="tag name">{user.data.name}</h2>
+          <h2 className="tag status">{user.data.status ? "Enter" : "Busy"}</h2>
         </div>
       );
 }
