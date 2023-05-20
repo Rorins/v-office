aea23a39d13d1982485bbd173c2f53c46fad56c7
Aurora <aurora.grippaudo@dotenv.it>
2023-05-20 22:12:07 +0200

    yey youtube search

--- src/components/YoutubeVideo.jsx
+++ src/components/YoutubeVideo.jsx
@@ -1,32 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import YouTube from "react-youtube";
+export default function YoutubeVideo() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [videoId, setVideoId] = useState("YB0SqL1A9NU");
 
-export default function YoutubeVideo({ videoId }) {
-    const opts = {
-      height: "390",
-      width: "640",
-      playerVars: {
-        autoplay: 0,
-      },
-    };
-  
-    return (
-    <div className="youtube_container">
-         <YouTube videoId={videoId} opts={opts} /> 
-    <div>
+  //Options
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
 
-<form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search video" required />
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
-
-    </div>     
-    </div>
+  //Search videos
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCFRuSs9j6E4PF-UWVHzohH3sLoR1VpNX8&q=${searchQuery}&part=snippet&type=video`
     );
-  }
+    const data = await response.json();
+    setSearchResults(data.items);
+  };
+
+  //Id handler
+  const handleId = (videoId) => {
+    setVideoId(videoId);
+  };
+
+  return (
+    //Main Youtube
+    <div className="youtube_container">
+      <YouTube videoId={videoId} opts={opts} />
+      <div>
+        <form onSubmit={handleSearch}>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search video"
+              required
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+        {/* Display search results */}
+        <div>
+          {searchResults.map((result) => (
+            <div className="result"  onClick={() => handleId(result.id.videoId)} key={result.id.videoId}>
+              <h3>{result.snippet.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
