f3dc5e30bf85df9d4295d548555247548149887c
Aurora <aurora.grippaudo@dotenv.it>
2023-05-22 00:21:19 +0200

    searchbar

--- src/components/YoutubeVideo.jsx
+++ src/components/YoutubeVideo.jsx
@@ -70,7 +70,19 @@ export default function YoutubeVideo() {
     }
   };
 
-  // ...
+   //Search videos
+   const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCFRuSs9j6E4PF-UWVHzohH3sLoR1VpNX8&q=${searchQuery}&part=snippet&type=video`
+    );
+    const data = await response.json();
+    setSearchResults(data.items);
+  };
+  //Id handler
+  const handleId = (videoId) => {
+    setVideoId(videoId);
+  };
 
   // YOUTUBE SYNC
 
@@ -150,6 +162,7 @@ export default function YoutubeVideo() {
     }
   };
 
+  //Visitor youtube check for updates
   useEffect(() => {
     if (!isThisMyRoom) {
       intervalRef.current = setInterval(async () => {
@@ -161,6 +174,10 @@ export default function YoutubeVideo() {
           const snapshot = await getDoc(docRef); // Declare a separate variable to capture the snapshot
           if (snapshot.exists()) {
             const data = snapshot.data();
+
+            // controllare data.videoId e aggiornare player se diverso da quello nello state
+            // quindi poi lo cambiamo nello state e nel player
+
             const ownerVideoStatus = data.videostatus;
 
             if (ownerVideoStatus === "play") {
@@ -216,15 +233,62 @@ export default function YoutubeVideo() {
   return (
     // Main Youtube
     <Draggable>
-      <div className="youtube_container">
-        <YouTube
-          onStateChange={handlePlayerStateChange}
-          videoId={videoId}
-          opts={opts}
-          onReady={handlePlayerReady} // Call handlePlayerReady when the player is ready
-        />
-        {/* ... */}
+    <div className="youtube_container">
+      <YouTube onStateChange={handlePlayerStateChange} videoId={videoId} opts={opts}  onReady={handlePlayerReady}/>
+      <div>
+        <form onSubmit={handleSearch}>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search video"
+              required
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-yellow-800 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+        {/* Display search results */}
+        <div>
+          {searchResults.map((result) => (
+            <div className="result"  onClick={() => handleId(result.id.videoId)} key={result.id.videoId}>
+              <h3>{result.snippet.title}</h3>
+            </div>
+          ))}
+        </div>
       </div>
+    </div>
     </Draggable>
   );
 }
+
