661dccc78c4eb9dd413c06a9d3b39c001e8fc55d
Aurora <aurora.grippaudo@dotenv.it>
2023-05-22 22:05:43 +0200

    chat fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import firebase_app from "@/firebase/config";
 import { collection, query, orderBy, limit, getDoc,  getFirestore ,addDoc, serverTimestamp} from "firebase/firestore";
 import { useCollectionData } from "react-firebase9-hooks/firestore";
+import getData from "@/firebase/firestore/getData";
 import { useAuthContext } from "@/context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane} from "@fortawesome/free-solid-svg-icons";
@@ -18,6 +19,22 @@ export default function Chat() {
   const { user, setUser } = useAuthContext();
   const [messages] = useCollectionData(queryRef, { idField: 'id' });
   const [formValue,setFormValue] = useState('')
+  const [userName, setUserName] = useState('');
+  const focus = useRef();
+
+   
+  //Retrieve single user data
+  const getUser = async () =>{
+    const {uid} = user;
+    const { result, error } = await getData('users', uid)
+    setUserName(result.name);
+}
+
+  //Retrieve user name on mounted
+  React.useEffect(()=>{
+    getUser()
+    }, [])
+
 
   const sendMessage = async(e) => {
     e.preventDefault()
@@ -28,9 +45,11 @@ export default function Chat() {
       text: formValue,
       createdAt: new Date(),
       uiduser: uid,
+      userName: userName,
     });
 
     setFormValue('')
+    focus.current.scrollIntoView({ behavior: 'smooth'})
   }
  
   return (
@@ -39,6 +58,8 @@ export default function Chat() {
     <span className="office_chat">Office Chat</span>
   <div className="messagesbox flex-grow h-64 w-72  overflow-y-auto">
     {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+
+    <div ref={focus}></div>
   </div>
 
   <form  onSubmit={sendMessage}>
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -4,7 +4,7 @@ import { useAuthContext } from "@/context/AuthContext";
 
 export default function ChatMessage(props) {
     const { user, setUser } = useAuthContext();
-    const{text, uiduser} = props.message
+    const{text, uiduser, userName} = props.message
 
    let uid;
     if (user && user.uid !== null) {
@@ -17,6 +17,7 @@ export default function ChatMessage(props) {
   
   return (
     <div className= {`message ${messageType}`}>
+      <span className="user_message">{userName}</span>
        <p className="max-w-xs overflow-hidden overflow-ellipsis">
        <span className="whitespace-pre-wrap">{text}</span>
         </p>
